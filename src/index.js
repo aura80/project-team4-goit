90480588d76c7e9da87edbd8b14cdfa2f282ae9f
User <96388713+aura80@users.noreply.github.com>
2024-12-12 19:54:08 +0200

    third commit - sections added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-import './js/header';
+import './js/hero-filter';
+import './js/hero-search';
+import './js/booster';
+import './js/booster-api';
+import './js/buy-tickets';
+import './js/modal';
+import './js/modal-team';
+import './js/more-authors';
