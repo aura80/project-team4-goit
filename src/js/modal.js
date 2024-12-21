9222bbf67e2e2f15cb35c994ad6e8d2bd76c7a67
Alecu Marco <136191501+MarcoPolo236@users.noreply.github.com>
2024-12-21 16:00:29 +0200

    more authors functionality added

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,4 +1,4 @@
-// modal.js
+import updatePage from './main.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const eventCardsContainer = document.getElementById('event-cards');
@@ -10,104 +10,122 @@ document.addEventListener('DOMContentLoaded', () => {
   // Open modal
   eventCardsContainer.addEventListener('click', async event => {
     const eventCard = event.target.closest('.event-card');
+    let eventName;
+
     if (eventCard) {
       const eventId = eventCard.getAttribute('data-id');
-
       if (eventId) {
         try {
-
           // Show spinner at modal load
+          const spinner = document.getElementById('spinner'); // Assuming spinner exists in DOM
           spinner.style.display = 'block';
 
           // Fetch event details from API
-          const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events/${eventId}.json?apikey=ABgCFeGHE7lHgwV23hsJAFL80GX9ypoh`);
+          const response = await fetch(
+            `https://app.ticketmaster.com/discovery/v2/events/${eventId}.json?apikey=ABgCFeGHE7lHgwV23hsJAFL80GX9ypoh`
+          );
           if (!response.ok) {
             throw new Error('Network response was not ok');
           }
-          const eventData = await response.json();
 
-          const eventName = eventData.name || '';
+          const eventData = await response.json();
+          eventName = eventData.name || '';
+
           const eventDate = eventData.dates?.start?.localDate || '';
           const eventTime = eventData.dates?.start?.localTime || '';
           const eventLocation = eventData._embedded?.venues?.[0]?.name || '';
-          const eventImage = eventData.images?.[0]?.url || 'https://via.placeholder.com/427x326?text=No+Image';
+          const eventImage =
+            eventData.images?.[0]?.url ||
+            'https://via.placeholder.com/427x326?text=No+Image';
           const ticketPrices = eventData.priceRanges || [];
           const buyStandardTicketUrl = eventData.url || '';
           const buyVipTicketUrl = `${eventData._embedded?.venues?.[0]?.url}?addOnType=VIP`;
-          
-          let minPrice = Math.min(ticketPrices[0]?.min ?? Infinity, ticketPrices[1]?.min ?? Infinity);
-          let maxPrice = Math.max(ticketPrices[0]?.max ??-Infinity, ticketPrices[1]?.max ??-Infinity);
+
+          let minPrice = Math.min(
+            ticketPrices[0]?.min ?? Infinity,
+            ticketPrices[1]?.min ?? Infinity
+          );
+          let maxPrice = Math.max(
+            ticketPrices[0]?.max ?? -Infinity,
+            ticketPrices[1]?.max ?? -Infinity
+          );
 
           if (modalEventDetails) {
             modalEventDetails.innerHTML = `
-                <div class="pic-circle">
+              <div class="pic-circle">
+                <img src="${eventImage}" alt="${eventName}">
+              </div>
+              <div class="event-content">
+                <div class="pic">
                   <img src="${eventImage}" alt="${eventName}">
                 </div>
-                <div class="event-content">
-                  <div class="pic">
-                    <img src="${eventImage}" alt="${eventName}">
-                  </div>
-                  <div class="modal-details">
-                    <div class="modal-info">
-                      <strong>INFO</strong>
-                      <div class="event-info">
-                      <p>${eventData.info || 'No additional information available.'}</p>
-                      </div>
+                <div class="modal-details">
+                  <div class="modal-info">
+                    <strong>INFO</strong>
+                    <div class="event-info">
+                      <p>${
+                        eventData.info || 'No additional information available.'
+                      }</p>
                     </div>
-                    <div class="when">
-                      <strong>WHEN</strong>
-                      <div class="when-info">
+                  </div>
+                  <div class="when">
+                    <strong>WHEN</strong>
+                    <div class="when-info">
                       <p>${eventDate}</p>
                       <p>${eventTime} (${eventLocation})</p>
-                      </div>
                     </div>
-                    <div class="where">
-                      <strong>WHERE</strong>
-                      <div class="where-info">
+                  </div>
+                  <div class="where">
+                    <strong>WHERE</strong>
+                    <div class="where-info">
                       <p>${eventLocation}</p>
-                      </div>
                     </div>
                   </div>
                 </div>
-                <div class="who">
-                  <strong>WHO</strong>
-                  <p>${eventName}</p>
-                </div>
-                <div class="prices">
-                  <strong>PRICES</strong>
-                  ${
-                    ticketPrices.length !== 0
-                      ? `
-                  <div class="prices-container">
-                    <div class="price-item">
-                    <div class="price-info">
-                      <div class="barcode"></div> <!-- Placeholder for barcode image -->
-                      <div class="standard-price">
-                        <p>STANDARD: $${minPrice} ${ticketPrices[0]?.currency} - $${ticketPrices[0]?.max} ${ticketPrices[0]?.currency}</p>
-                      </div>
-                    </div>
+              </div>
+              <div class="who">
+                <strong>WHO</strong>
+                <p>${eventName}</p>
+              </div>
+              <div class="prices">
+                <strong>PRICES</strong>
+                ${
+                  ticketPrices.length !== 0
+                    ? `
+                    <div class="prices-container">
+                      <div class="price-item">
+                        <div class="price-info">
+                          <div class="barcode"></div> <!-- Placeholder for barcode image -->
+                          <div class="standard-price">
+                            <p>STANDARD: $${minPrice} ${
+                        ticketPrices[0]?.currency
+                      } - $${ticketPrices[0]?.max} ${
+                        ticketPrices[0]?.currency
+                      }</p>
+                          </div>
+                        </div>
                         <button id="buyStandardButton" class="buy-button-standard" data-url="${buyStandardTicketUrl}">Buy Tickets</button>
                       </div>
-                    ${
-                      ticketPrices[1]
-                        ? `
-                      <div class="price-item">
-                      <div class="price-info">
-                        <div class="barcode"></div> <!-- Placeholder for barcode image -->
-                        <div class="vip-price">
-                          <p>VIP: $${maxPrice} ${ticketPrices[1]?.currency} - $${ticketPrices[1]?.max} ${ticketPrices[1]?.currency}</p>
-                          </div>
-                          </div>
-                          <button id="buyVipButton" class="buy-button-vip" data-url="${buyVipTicketUrl}">Buy Tickets</button>
-                      </div>`
-                        : ''
-                    }
-                  </div>`
-                      : 'There are no tickets available for this event'
-                  }
-                </div>
-                <button id="more-from-authors" class="more-button">MORE FROM THIS AUTHOR</button>
-              `;
+                      ${
+                        ticketPrices[1]
+                          ? `
+                          <div class="price-item">
+                            <div class="price-info">
+                              <div class="barcode"></div> <!-- Placeholder for barcode image -->
+                              <div class="vip-price">
+                                <p>VIP: $${maxPrice} ${ticketPrices[1]?.currency} - $${ticketPrices[1]?.max} ${ticketPrices[1]?.currency}</p>
+                              </div>
+                            </div>
+                            <button id="buyVipButton" class="buy-button-vip" data-url="${buyVipTicketUrl}">Buy Tickets</button>
+                          </div>`
+                          : ''
+                      }
+                    </div>`
+                    : 'There are no tickets available for this event'
+                }
+              </div>
+              <button id="more-from-authors" class="more-button" data-name="${eventName}">MORE FROM THIS AUTHOR</button>
+            `;
           }
 
           modal.style.display = 'block';
@@ -118,6 +136,14 @@ document.addEventListener('DOMContentLoaded', () => {
             spinner.style.display = 'none';
           }, 1200);
 
+          // Attach the event listener for the more-from-authors button here
+          const modalMoreBtn = document.getElementById('more-from-authors');
+          modalMoreBtn.addEventListener('click', () => {
+            const searchEvent = eventName; // Use the correct author name
+            updatePage(searchEvent);
+            modal.style.display = 'none';
+            modalOverlay.style.display = 'none';
+          });
         } catch (error) {
           console.error('Failed to fetch event details:', error);
         }
